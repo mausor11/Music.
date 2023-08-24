f80d586ee2df0249b11b04f72f12715ba079f41a
mausor11 <bartosz.danko@gmail.com>
2023-08-24 21:10:18 +0200

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module Music {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires java.sql;
+
+    opens org.main to javafx.fxml, javafx.graphics, javafx.controls, javafx.media, java.sql;
+
+    exports org.main to javafx.controls, javafx.fxml, javafx.graphics, javafx.media;
+}
