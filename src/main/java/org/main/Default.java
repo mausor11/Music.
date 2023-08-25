f5a11ff7c311040407824ad74f3fc138a1f66cb4
mausor11 <bartosz.danko@gmail.com>
2023-08-25 15:47:34 +0200

    tiles icons

--- src/main/java/org/main/Default.java
+++ src/main/java/org/main/Default.java
@@ -2,18 +2,28 @@ package org.main;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Default {
 /** Size **/
     public final static int MIN_WIDTH = 1320;
     public final static int MIN_HEIGHT = 815;
-
-
+/** Dark Theme Colors **/
+    public final static Color BACKGROUND_COLOR = Color.web("#030303");
+    public final static Color TILES_COLOR = Color.web("#1D1919");
+    public final static Color LIGHT_TILES_COLOR = Color.web("#302C2C");
+    public final static Color BROWN_4 = Color.web("#553939");
+    public final static Color BROWN_3 = Color.web("#704F4F");
+    public final static Color BROWN_2 = Color.web("#937272");
+    public final static Color BROWN_1 = Color.web("#A77979");
+    public final static Color FONT_COLOR = Color.web("#E4CFCF");
     public static Scene mainViewScene; static {
         try {
             mainViewScene = new Scene(new FXMLLoader(Default.class.getResource("fxml/MainView.fxml")).load());
+            mainViewScene.getStylesheets().add(Objects.requireNonNull(Objects.requireNonNull(Default.class.getResource("css/darkTheme.css")).toExternalForm()));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/main/controllers/MainViewController.java
+++ src/main/java/org/main/controllers/MainViewController.java
@@ -1,6 +1,54 @@
 package org.main.controllers;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+
 public class MainViewController {
+    @FXML
+    StackPane homeTile;
+    @FXML
+    Rectangle homeCover;
+    private boolean isAnimated = false;
+/**<p>0 - Home Tile</p>
+ * <p>1 - Playlists Tile</p>
+ * <p>2 - Albums Tile</p>
+ * <p>3 - Favourite Tile</p> **/
+    private int actualTile = 0;
+    private int prevTile = 0;
 
+    @FXML
+    public void initialize() {
+    }
 
+    public void setHomeTile() {
+        if(actualTile != 0) {
+            actualTile = 0;
+        }
+    }
+
+    public void setPlaylistsTile() {
+        if(actualTile != 1) {
+            actualTile = 1;
+        }
+
+    }
+
+    public void setAlbumsTile() {
+        if(actualTile != 2) {
+            actualTile = 2;
+        }
+    }
+
+    public void setFavouriteTile() {
+        if(actualTile != 3) {
+            actualTile = 3;
+        }
+    }
 }
