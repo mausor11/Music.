b0545bd67e910998773254e078367d539a67aaf5
mausor11 <bartosz.danko@gmail.com>
2024-01-09 18:54:35 +0100

    Jar: working (not everything)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module Music {
     requires java.sql;
 
     opens org.main.controllers to javafx.fxml, javafx.graphics, javafx.controls, javafx.media, java.sql;
+    opens org.main to javafx.fxml;
 
     exports org.main to javafx.controls, javafx.fxml, javafx.graphics, javafx.media;
 }
